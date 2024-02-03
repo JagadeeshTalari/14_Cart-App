4587346411337a395c3f69f57e7532a75849ea36
Jagadeesh Talari <93560813+JagadeeshTalari@users.noreply.github.com>
2024-02-03 16:21:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -9,17 +8,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <AppProvider>
       <App />
     </AppProvider>
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import AppContext from "./GlobalContext/AppContext";
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AppContext>
-      <App />
-    </AppContext>
->>>>>>> origin/main
-  </React.StrictMode>
-);
+
